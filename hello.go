6ac819e867c53b7139d5aaf2cd33a10731ecf635
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-01-28 09:15:26 +0900

    snapshot

--- hello.go
+++ hello.go
@@ -1,32 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"encoding/binary"
 	"github.com/google/hprof-parser/hprofdata"
 	"github.com/google/hprof-parser/parser"
+	"github.com/hashicorp/logutils"
 	"io"
 	"log"
 	"os"
 	"sort"
 )
 
-type counters struct {
-	countJNIGlobal   uint64
-	countJNILocal    uint64
-	countJavaFrame   uint64
-	countStickyClass uint64
-	countThreadObj   uint64
-	countLoadClass   uint64
+type Seen struct {
+	seen map[uint64]bool
+}
+
+func NewSeen() *Seen {
+	m := new(Seen)
+	m.seen = make(map[uint64]bool)
+	return m
+}
+
+func (s Seen) Add(id uint64) {
+	s.seen[id] = true
+}
+
+func (s Seen) Remove(id uint64) {
+	delete(s.seen, id)
+}
+
+func (s Seen) HasKey(id uint64) bool {
+	_, ok := s.seen[id]
+	return ok
+}
+
+func (s Seen) Size() int {
+	return len(s.seen)
 }
 
 type HeapDumpAnalyzer struct {
-	nameId2string             map[uint64]string
-	classObjectId2classNameId map[uint64]uint64
-	classObjectId2objectIds   map[uint64][]uint64
-	classObjectId2classDump   map[uint64]*hprofdata.HProfClassDump
-	arrayObjectId2bytes       map[uint64]int
-	countClassDump            uint64 // Total Classes
-	objectId2instanceDump     map[uint64]*hprofdata.HProfInstanceDump
+	nameId2string                    map[uint64]string
+	classObjectId2classNameId        map[uint64]uint64
+	classObjectId2objectIds          map[uint64][]uint64
+	classObjectId2classDump          map[uint64]*hprofdata.HProfClassDump
+	arrayObjectId2primitiveArrayDump map[uint64]*hprofdata.HProfPrimitiveArrayDump
+	arrayObjectId2objectArrayDump    map[uint64]*hprofdata.HProfObjectArrayDump
+	countClassDump                   uint64 // Total Classes
+	objectId2instanceDump            map[uint64]*hprofdata.HProfInstanceDump
+	sizeCache                        map[uint64]uint64
 }
 
 func NewHeapDumpAnalyzer() *HeapDumpAnalyzer {
@@ -35,8 +56,10 @@ func NewHeapDumpAnalyzer() *HeapDumpAnalyzer {
 	m.classObjectId2classNameId = make(map[uint64]uint64)
 	m.classObjectId2objectIds = make(map[uint64][]uint64)
 	m.classObjectId2classDump = make(map[uint64]*hprofdata.HProfClassDump)
-	m.arrayObjectId2bytes = make(map[uint64]int)
+	m.arrayObjectId2primitiveArrayDump = make(map[uint64]*hprofdata.HProfPrimitiveArrayDump)
+	m.arrayObjectId2objectArrayDump = make(map[uint64]*hprofdata.HProfObjectArrayDump)
 	m.objectId2instanceDump = make(map[uint64]*hprofdata.HProfInstanceDump)
+	m.sizeCache = make(map[uint64]uint64)
 	return m
 }
 
@@ -53,7 +76,6 @@ func (a HeapDumpAnalyzer) Scan(heapFilePath string) error {
 		return nil
 	}
 
-	cs := &counters{}
 	var prev int64
 	for {
 		record, err := p.ParseRecord()
@@ -87,7 +109,6 @@ func (a HeapDumpAnalyzer) Scan(heapFilePath string) error {
 			a.classObjectId2classNameId[o.GetClassObjectId()] = o.GetClassNameId()
 			log.Printf("%v=%v", o.GetClassObjectId(),
 				a.nameId2string[o.GetClassNameId()])
-			cs.countLoadClass += 1
 		case *hprofdata.HProfRecordFrame:
 			// stack frame.
 			//key = o.GetStackFrameId()
@@ -95,7 +116,6 @@ func (a HeapDumpAnalyzer) Scan(heapFilePath string) error {
 			// stack trace
 			//key = uint64(o.GetStackTraceSerialNumber())
 		case *hprofdata.HProfRecordHeapDumpBoundary:
-			log.Printf("DO IT")
 			break
 		case *hprofdata.HProfClassDump:
 			//key = o.GetClassObjectId()
@@ -105,18 +125,14 @@ func (a HeapDumpAnalyzer) Scan(heapFilePath string) error {
 			a.classObjectId2classDump[o.ClassObjectId] = o
 			a.countClassDump += 1
 		case *hprofdata.HProfInstanceDump: // HPROF_GC_INSTANCE_DUMP
-			//key = o.GetObjectId()
-			classNameId := a.classObjectId2classNameId[o.GetClassObjectId()]
-			className := a.nameId2string[classNameId]
-			log.Printf("INSTANCE! className=%s", className)
 			a.classObjectId2objectIds[o.ClassObjectId] = append(a.classObjectId2objectIds[o.ClassObjectId], o.ObjectId)
 			a.objectId2instanceDump[o.ObjectId] = o
 		case *hprofdata.HProfObjectArrayDump:
-			//key = o.GetArrayObjectId()
-		case *hprofdata.HProfPrimitiveArrayDump:
-			//key = o.GetArrayObjectId()
 			arrayObjectId := o.GetArrayObjectId()
-			a.arrayObjectId2bytes[arrayObjectId] = len(o.GetValues())
+			a.arrayObjectId2objectArrayDump[arrayObjectId] = o
+		case *hprofdata.HProfPrimitiveArrayDump:
+			arrayObjectId := o.GetArrayObjectId()
+			a.arrayObjectId2primitiveArrayDump[arrayObjectId] = o
 		case *hprofdata.HProfRootJNIGlobal:
 			//key = cs.countJNIGlobal
 			//cs.countJNIGlobal++
@@ -140,7 +156,32 @@ func (a HeapDumpAnalyzer) Scan(heapFilePath string) error {
 	return nil
 }
 
-func (a HeapDumpAnalyzer) Dump() {
+func (a HeapDumpAnalyzer) DumpInclusiveRanking() {
+	var classObjectIds []uint64
+	for k, _ := range a.classObjectId2objectIds {
+		classObjectIds = append(classObjectIds, k)
+	}
+
+	var classObjectId2objectSize = map[uint64]uint64{}
+	for classObjectId, objectIds := range a.classObjectId2objectIds {
+		for _, objectId := range objectIds {
+			classObjectId2objectSize[classObjectId] += a.sizeInstance(objectId, NewSeen())
+		}
+	}
+
+	sort.Slice(classObjectIds, func(i, j int) bool {
+		return classObjectId2objectSize[classObjectIds[i]] < classObjectId2objectSize[classObjectIds[j]]
+	})
+	for _, classObjectId := range classObjectIds {
+		classNameId := a.classObjectId2classNameId[classObjectId]
+		name := a.nameId2string[classNameId]
+		log.Printf("[INFO] %d\t= %s\n",
+			classObjectId2objectSize[classObjectId],
+			name)
+	}
+}
+
+func (a HeapDumpAnalyzer) DumpExclusiveRanking() {
 	var classObjectIds []uint64
 	for k, _ := range a.classObjectId2objectIds {
 		classObjectIds = append(classObjectIds, k)
@@ -151,61 +192,310 @@ func (a HeapDumpAnalyzer) Dump() {
 	for _, classObjectId := range classObjectIds {
 		classNameId := a.classObjectId2classNameId[classObjectId]
 		name := a.nameId2string[classNameId]
-		fmt.Printf("%d\t= %s\n",
+		log.Printf("[INFO] %d\t= %s\n",
 			len(a.classObjectId2objectIds[classObjectId]),
 			name)
 	}
+}
 
+func (a HeapDumpAnalyzer) ShowTotalClasses() {
 	log.Printf("Total Classes=%v", a.countClassDump)
 }
 
-func (a HeapDumpAnalyzer) sizeInstance(objectId uint64) {
+func (a HeapDumpAnalyzer) sizeInstance(objectId uint64, seen *Seen) uint64 {
+	if cacheEntry, ok := a.sizeCache[objectId]; ok {
+		return cacheEntry
+	}
+
+	if seen == nil {
+		panic("Missing seen")
+	}
+	if seen.HasKey(objectId) { // recursive counting.
+		log.Printf("[INFO] Recursive counting occurred: %v", objectId)
+		return 0
+	}
+
+	log.Printf("[DEBUG] sizeInstance() oid=%d seen=%v",
+		objectId,
+		seen)
+
+	seen.Add(objectId)
+	defer seen.Remove(objectId)
+
 	instanceDump := a.objectId2instanceDump[objectId]
+	if instanceDump != nil {
+		return a.calcObjectSize(instanceDump, objectId, seen)
+	}
+
+	objectArrayDump := a.arrayObjectId2objectArrayDump[objectId]
+	if objectArrayDump != nil {
+		return a.calcObjectArraySize(objectArrayDump, seen)
+	}
+
+	primitiveArrayDump := a.arrayObjectId2primitiveArrayDump[objectId]
+	if primitiveArrayDump != nil {
+		return a.calcPrimitiveArraySize(primitiveArrayDump)
+	}
+
+	classDump := a.classObjectId2classDump[objectId]
+	if classDump != nil {
+		return a.calcClassSize(classDump, seen)
+	}
+
+	log.Fatalf(
+		"[ERROR] Unknown instance: objectId=%v instanceDump=%v str=%v primArray=%v objArray=%v class=%v",
+		objectId,
+		instanceDump,
+		a.nameId2string[objectId],
+		a.arrayObjectId2primitiveArrayDump[objectId],
+		a.arrayObjectId2objectArrayDump[objectId],
+		a.classObjectId2classNameId[objectId])
+	return 0 // should not reach here
+}
+
+func (a HeapDumpAnalyzer) calcObjectSize(
+	instanceDump *hprofdata.HProfInstanceDump,
+	objectId uint64,
+	seen *Seen) uint64 {
 	classDump := a.classObjectId2classDump[instanceDump.ClassObjectId]
-	log.Printf("%v", classDump)
+	classNameId := a.classObjectId2classNameId[classDump.ClassObjectId]
+	log.Printf("[DEBUG] calcObjectSize(name=%v) oid=%d seen=%v",
+		a.nameId2string[classNameId],
+		objectId,
+		seen)
 
 	// instance field を舐めてサイズを計算する。super があればそこも全てみる。
-	size := 0
-	for _, field := range classDump.InstanceFields {
-		switch field.Type {
+
+	size := uint64(len(instanceDump.GetValues()))
+	values := instanceDump.GetValues()
+	// 読み込んだバイト数, サイズ
+	for {
+		nIdx, nSize := a.scanInstance(classDump.InstanceFields, values, seen)
+		size += nSize
+		values = values[nIdx:]
+		if classDump.SuperClassObjectId == 0 {
+			break
+		} else {
+			classDump = a.classObjectId2classDump[classDump.SuperClassObjectId]
+			log.Printf("[DEBUG]  checking super class %v",
+				a.nameId2string[a.classObjectId2classNameId[classDump.ClassObjectId]])
+		}
+	}
+
+	a.sizeCache[objectId] = size
+	return size
+}
+
+type Fieldish interface {
+	GetType() hprofdata.HProfValueType
+	GetNameId() uint64
+}
+
+func (a HeapDumpAnalyzer) scanInstance(
+	fields []*hprofdata.HProfClassDump_InstanceField,
+	values []byte,
+	seen *Seen) (int, uint64) {
+	var nameIds []uint64
+	var types []hprofdata.HProfValueType
+	for _, field := range fields {
+		nameIds = append(nameIds, field.NameId)
+		types = append(types, field.Type)
+	}
+	return a.scanFields(nameIds, types, values, seen)
+}
+
+func (a HeapDumpAnalyzer) scanStaticFields(
+	fields []*hprofdata.HProfClassDump_StaticField,
+	seen *Seen) uint64 {
+	size := uint64(0)
+	for _, field := range fields {
+		switch field.GetType() {
 		case hprofdata.HProfValueType_OBJECT:
-			log.Printf("TODO: object scan is not available yet.")
+			// TODO 32bit support(特にやる気なし)
+			objectId := field.GetValue()
+			log.Printf("[TRACE]  object field: name=%v oid=%v",
+				a.nameId2string[field.GetNameId()],
+				objectId)
+			if objectId == 0 {
+				// the field contains null
+			} else {
+				size += a.sizeInstance(objectId, seen)
+			}
+			size += 8
 		// Boolean. Takes 0 or 1. One byte.
 		case hprofdata.HProfValueType_BOOLEAN:
+			log.Printf("[TRACE]  Boolean Field: %v", a.nameId2string[field.GetNameId()])
 			size += 1
-			// Character. Two bytes.
+		// Character. Two bytes.
 		case hprofdata.HProfValueType_CHAR:
+			log.Printf("[TRACE]  char Field: %v", a.nameId2string[field.GetNameId()])
 			size += 2
-			// Float. 4 bytes
+		// Float. 4 bytes
 		case hprofdata.HProfValueType_FLOAT:
+			log.Printf("[TRACE]  float Field: %v", a.nameId2string[field.GetNameId()])
 			size += 4
-			// Double. 8 bytes.
+		// Double. 8 bytes.
 		case hprofdata.HProfValueType_DOUBLE:
+			log.Printf("[TRACE]  double Field: %v", a.nameId2string[field.GetNameId()])
 			size += 8
-			// Byte. One byte.
+		// Byte. One byte.
 		case hprofdata.HProfValueType_BYTE:
+			log.Printf("[TRACE]  byte Field: %v", a.nameId2string[field.GetNameId()])
 			size += 1
-			// Short. Two bytes.
+		// Short. Two bytes.
 		case hprofdata.HProfValueType_SHORT:
+			log.Printf("[TRACE]  short Field: %v %v",
+				a.nameId2string[field.GetNameId()],
+				int16(field.GetValue()))
 			size += 2
-			// Integer. 4 bytes.
+		// Integer. 4 bytes.
 		case hprofdata.HProfValueType_INT:
+			log.Printf("[TRACE]  int Field: %v, %v",
+				a.nameId2string[field.GetNameId()],
+				int32(field.GetValue()))
 			size += 4
-			// Long. 8 bytes.
+		// Long. 8 bytes.
 		case hprofdata.HProfValueType_LONG:
+			log.Printf("[TRACE]  long Field: %v",
+				a.nameId2string[field.GetNameId()])
 			size += 8
 		default:
-			log.Fatalf("Unknown value type: %x", field.Type)
+			log.Fatalf("Unknown value type: %x", field.GetType())
+		}
+	}
+	return size
+}
+
+func (a HeapDumpAnalyzer) scanFields(
+	nameIds []uint64,
+	types []hprofdata.HProfValueType,
+	values []byte,
+	seen *Seen) (int, uint64) {
+
+	size := uint64(0)
+	idx := 0
+
+	for i, nameId := range nameIds {
+		switch types[i] {
+		case hprofdata.HProfValueType_OBJECT:
+			// TODO 32bit support(特にやる気なし)
+			objectIdBytes := values[idx : idx+8]
+			objectId := binary.BigEndian.Uint64(objectIdBytes)
+			log.Printf("[TRACE]  object field: name=%v oid=%v",
+				a.nameId2string[nameId],
+				objectId)
+			if objectId == 0 {
+				// the field contains null
+			} else {
+				size += a.sizeInstance(objectId, seen)
+			}
+			idx += 8
+		// Boolean. Takes 0 or 1. One byte.
+		case hprofdata.HProfValueType_BOOLEAN:
+			log.Printf("[TRACE]  Boolean Field: %v", a.nameId2string[nameId])
+			idx += 1
+		// Character. Two bytes.
+		case hprofdata.HProfValueType_CHAR:
+			log.Printf("[TRACE]  char Field: %v", a.nameId2string[nameId])
+			idx += 2
+		// Float. 4 bytes
+		case hprofdata.HProfValueType_FLOAT:
+			log.Printf("[TRACE]  float Field: %v", a.nameId2string[nameId])
+			idx += 4
+		// Double. 8 bytes.
+		case hprofdata.HProfValueType_DOUBLE:
+			log.Printf("[TRACE]  double Field: %v", a.nameId2string[nameId])
+			idx += 8
+		// Byte. One byte.
+		case hprofdata.HProfValueType_BYTE:
+			log.Printf("[TRACE]  byte Field: %v", a.nameId2string[nameId])
+			idx += 1
+		// Short. Two bytes.
+		case hprofdata.HProfValueType_SHORT:
+			log.Printf("[TRACE]  short Field: %v %v",
+				a.nameId2string[nameId],
+				int16(binary.BigEndian.Uint16(values[idx:idx+2])))
+			idx += 2
+		// Integer. 4 bytes.
+		case hprofdata.HProfValueType_INT:
+			log.Printf("[TRACE]  int Field: %v, %v",
+				a.nameId2string[nameId],
+				int32(binary.BigEndian.Uint32(values[idx:idx+4])))
+			idx += 4
+		// Long. 8 bytes.
+		case hprofdata.HProfValueType_LONG:
+			log.Printf("[TRACE]  long Field: %v",
+				a.nameId2string[nameId])
+			idx += 8
+		default:
+			log.Fatalf("Unknown value type: %x", types[i])
+		}
+	}
+
+	return idx, size
+}
+
+func (a HeapDumpAnalyzer) CalculateSizeOfInstancesByName(targetName string) {
+	// Debugging
+	for classObjectId, objectIds := range a.classObjectId2objectIds {
+		name := a.nameId2string[a.classObjectId2classNameId[classObjectId]]
+		if name == targetName {
+			for _, objectId := range objectIds {
+				seen := NewSeen()
+				i := a.sizeInstance(objectId, seen)
+				log.Printf("[INFO] Scanned %v", i)
+			}
+			return
+		}
+	}
+	log.Fatalf("[ERROR] %s not found", targetName)
+}
+
+func (a HeapDumpAnalyzer) CalculateSizeOfInstances() {
+	var classObjectIds []uint64
+	for k, _ := range a.classObjectId2objectIds {
+		classObjectIds = append(classObjectIds, k)
+	}
+	sort.Slice(classObjectIds, func(i, j int) bool {
+		return a.classObjectId2classNameId[classObjectIds[i]] < a.classObjectId2classNameId[classObjectIds[j]]
+	})
+
+	for _, classObjectId := range classObjectIds {
+		objectIds := a.classObjectId2objectIds[classObjectId]
+
+		for _, objectId := range objectIds {
+			seen := NewSeen()
+			i := a.sizeInstance(objectId, seen)
+			log.Printf("[INFO] Scaned %d", i)
 		}
 	}
 }
 
-func (a HeapDumpAnalyzer) CalculateSizeOfInstances() {
-	for _, objectIds := range a.classObjectId2objectIds {
-		for _, objectId := range objectIds {
-			a.sizeInstance(objectId)
+func (a HeapDumpAnalyzer) calcObjectArraySize(dump *hprofdata.HProfObjectArrayDump, seen *Seen) uint64 {
+	log.Printf("[DEBUG]      object array: %v len=%v",
+		dump.ArrayObjectId,
+		len(dump.GetElementObjectIds()))
+	objectIds := dump.GetElementObjectIds()
+	r := uint64(0)
+	for _, objectId := range objectIds {
+		if objectId != 0 {
+			r += a.sizeInstance(objectId, seen)
 		}
 	}
+	return r
+}
+
+func (a HeapDumpAnalyzer) calcPrimitiveArraySize(dump *hprofdata.HProfPrimitiveArrayDump) uint64 {
+	log.Printf("[DEBUG]      primitive array: %s %v",
+		dump.ElementType,
+		len(dump.Values))
+	return uint64(len(dump.Values))
+}
+
+func (a HeapDumpAnalyzer) calcClassSize(dump *hprofdata.HProfClassDump, seen *Seen) uint64 {
+	log.Printf("[DEBUG]      class: %v",
+		dump.ClassObjectId)
+	return a.scanStaticFields(dump.GetStaticFields(), seen)
 }
 
 func parseIt() error {
@@ -219,15 +509,28 @@ func parseIt() error {
 		return err
 	}
 
-	log.Printf("---- CalculateSizeOfInstances phase ----")
-	analyzer.CalculateSizeOfInstances()
+	log.Printf("[INFO] ---- CalculateSizeOfInstances phase ----")
+	//analyzer.CalculateSizeOfInstancesByName("java/security/BasicPermissionCollection")
+	analyzer.CalculateSizeOfInstancesByName("java/util/HashMap")
+	//analyzer.CalculateSizeOfInstancesByName("Foo")
+	//analyzer.CalculateSizeOfInstances()
 
-	analyzer.Dump()
+	analyzer.DumpInclusiveRanking()
+	//analyzer.DumpExclusiveRanking()
+	analyzer.ShowTotalClasses()
 
 	return nil
 }
 
 func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"TRACE", "DEBUG", "INFO", "WARN", "ERROR"},
+		MinLevel: logutils.LogLevel("TRACE"),
+		Writer:   os.Stderr,
+	}
+	log.SetOutput(filter)
+
 	err := parseIt()
 	if err != nil {
 		log.Fatal(err)
