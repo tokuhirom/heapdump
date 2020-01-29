ea1440c9d4231e096ac04ed860d9bc792efceee6
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-01-29 16:40:42 +0900

    split logger

--- hello.go
+++ hello.go
@@ -15,47 +15,6 @@ type Logger struct {
 	indent int
 }
 
-func NewLogger() *Logger {
-	m := new(Logger)
-	m.indent = 0
-	return m
-}
-
-func (a *Logger) Indent() {
-	a.indent = a.indent + 1
-}
-
-func (a *Logger) Dedent() {
-	a.indent = a.indent - 1
-}
-
-func (a *Logger) spaces() string {
-	r := ""
-	for i := 0; i < a.indent; i++ {
-		r += "  "
-	}
-	return r
-}
-
-func (a *Logger) Trace(msg string, v ...interface{}) {
-	log.Printf("[TRACE] "+a.spaces()+msg, v...)
-}
-
-func (a *Logger) Debug(msg string, v ...interface{}) {
-	log.Printf("[DEBUG] "+a.spaces()+msg, v...)
-}
-
-func (a *Logger) Info(msg string, v ...interface{}) {
-	log.Printf("[INFO] "+a.spaces()+msg, v...)
-}
-func (a *Logger) Warn(msg string, v ...interface{}) {
-	log.Printf("[WARN] "+a.spaces()+msg, v...)
-}
-
-func (a *Logger) Error(msg string, v ...interface{}) {
-	log.Printf("[ERROR] "+a.spaces()+msg, v...)
-}
-
 type Seen struct {
 	seen map[uint64]bool
 }
